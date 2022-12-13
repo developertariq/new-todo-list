3a50716b68988102d30d6c0d2191ee6dab47811e
developertariq <dev.tariqul.islam@gmail.com>
2022-12-13 22:40:04 +0600

    Fix 3

--- webpack.config.js
+++ webpack.config.js
@@ -36,13 +36,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: 'file-loader',
-        options: {
-          outputPath: '../fonts',
-        },
-      },
     ],
   },
 };
